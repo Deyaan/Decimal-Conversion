28d6ad2741f72d0a20643e267592ced77183a26a
Deyaan <65954790+Deyaan@users.noreply.github.com>
2021-04-04 15:48:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var num = 2463;
+var a = num.toString();
+var b = num.toString(2);
+var c = num.toString(3);
+var d = num.toString(4);
+var e = num.toString(5);
+var f = num.toString(6);
+var g = num.toString(7);
+var h = num.toString(8);
+var i = num.toString(9);
+var j = num.toString(16);
+
+    console.log("Decimal: " + a);
+    console.log("With Base as Two(Binary): "+b);
+    console.log("With Base three: "+c);
+    console.log("With Base Four: "+ d);
+    console.log("With Base five: "+e);
+    console.log("With Base six: "+f);
+    console.log("With Base seven: "+g);
+    console.log("With Base eight(Octal): "+h);
+    console.log("With Base nine: "+i);
+    console.log("With Base sixteen(Hexadecimal): "+j);
